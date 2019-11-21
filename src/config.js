29984b59ce5680ca7a989987bf2be522832ff7f5
Christopher Williams <cw@chriswillia.ms>
2019-11-20 21:00:11 -0600

    Adds dependencies and basic config for database migrations.

--- src/config.js
+++ src/config.js
@@ -1,4 +1,6 @@
 module.exports = {
     PORT: process.env.PORT || 8000,
-    NODE_ENV: process.env.NODE_ENV || 'development'
+    NODE_ENV: process.env.NODE_ENV || 'development',
+    DB_URL: process.env.DATABASE_URL || 'postgresql://journalbuddy@localhost/journal-buddy',
+    DB_TEST_URL: process.env.TEST_DB_URL || 'postgresql://journalbuddy@localhost/journal-buddy-test'
 }
