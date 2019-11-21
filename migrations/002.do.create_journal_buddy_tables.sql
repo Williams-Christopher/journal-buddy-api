2244c847973368084317581427530f7fd7f76b43
Christopher Williams <cw@chriswillia.ms>
2019-11-20 23:34:44 -0600

    Adds postgrator config and migration files.

---
+++ migrations/002.do.create_journal_buddy_tables.sql
@@ -0,0 +1,18 @@
+CREATE TABLE users (
+    id serial primary key,
+    user_name varchar(32) not null,
+    password varchar(32) not null,
+    email varchar(32),
+    created timestamp default now() not null
+);
+
+CREATE TABLE entries (
+    id serial primary key,
+    user_id integer not null references users(id) on delete cascade,
+    entry_id uuid default uuid_generate_v4() not null,
+    feeling smallint not null,
+    title varchar(50),
+    body text,
+    privacy smallint not null,
+    created timestamp default now() not null
+);
