7cc005c5cf732a63a1361e75ab9cb7d210b7b2d1
Christopher Williams <cw@chriswillia.ms>
2019-11-21 22:44:47 -0600

    User login and JWT issuing - implementation including configuration updates.

--- src/server.js
+++ src/server.js
@@ -1,5 +1,14 @@
+require('dotenv').config();
 const app = require('./app');
-const { PORT } = require('./config');
+const knex = require('knex');
+// Check out process.env.NODE_ENV to determine if DB_URL or DB_TEST_URL?
+const { PORT, DB_TEST_URL } = require('./config');
+
+const db = knex({
+    client: 'pg',
+    connection: DB_TEST_URL
+});
+app.set('db', db);
 
 app.listen(PORT, () => {
     if (process.env.NODE_ENV === 'development') {
