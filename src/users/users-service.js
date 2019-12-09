fe84e97b15c4fbff76ffde9c92517f93ea459b89
Christopher Williams <cw@chriswillia.ms>
2019-12-09 16:42:37 -0600

    Implements regex for email validation according to RFC 5322.

--- src/users/users-service.js
+++ src/users/users-service.js
@@ -1,6 +1,8 @@
 const xss = require('xss');
 const bcrypt = require('bcryptjs');
 const PASSWORD_REGEX = /(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#\$%\^&])(?=.{8,})[\S]+/;
+/* email regex from emailregex.com, the RFC 5322 Official Standard version */
+const EMAIL_REGEX = /(?:[a-z0-9!#$%&'*+\/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+\/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/;
 
 const UsersService = {
     checkUserNameExists(db, user_name) {
@@ -34,6 +36,14 @@ const UsersService = {
         if(!PASSWORD_REGEX.test(password)) {
             return 'Password does not meet complexity requirements'
         }
+    },
+
+    emailValidation(email) {
+        if(EMAIL_REGEX.test(email)) {
+            return true;
+        };
+
+        return false;
     },
 
     serializeNewUser(newUser) {
